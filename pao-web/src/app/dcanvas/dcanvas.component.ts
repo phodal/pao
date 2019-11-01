3f2d55fa1a74edb221a6ba045569ed4e89f08aa4
Phodal HUANG <h@phodal.com>
2019-11-01 22:23:46 +0800

    feat: add svg.js

--- pao-web/src/app/dcanvas/dcanvas.component.ts
+++ pao-web/src/app/dcanvas/dcanvas.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {SVG} from '@svgdotjs/svg.js';
+import '@svgdotjs/svg.draggable.js'
 
 @Component({
   selector: 'app-dcanvas',
   templateUrl: './dcanvas.component.html',
   styleUrls: ['./dcanvas.component.less']
 })
-export class DcanvasComponent implements OnInit {
+export class DcanvasComponent implements OnInit, AfterViewInit {
   data = {
     "name": "庖丁解牛系统",
     "objects": [
@@ -29,4 +31,15 @@ export class DcanvasComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    this.drawTree();
+  }
+
+
+  private drawTree() {
+    var draw = SVG().addTo('#drawing');
+    var rect = draw.rect(100, 100).fill('#f06').move(20, 20)
+
+    rect.draggable();
+  }
 }
